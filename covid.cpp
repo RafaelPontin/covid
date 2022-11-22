d294ef1360348127c395e3111e4b0319c0fbd37b
rafael pontin <rfmpontin@gmail.com>
2022-11-22 20:45:38 -0300

    Main * novo login * gravar dados

--- covid.cpp
+++ covid.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include<conio.h>
 
 
 int main(){
@@ -11,11 +12,23 @@ int main(){
     char teste [2] = " ";
     
     
+    char nome[20] = " ";
+    char stringConc[200] = " ";
+    
+    
     int loginValido = 0;
+    int opcao = 1;
 
     do 
     {
-         system("cls");
+    
+		system("cls");
+		
+    	printf("===============================================\n");
+    	printf("================Sistema_Covid==================\n");
+    	printf("===============================================\n\n");
+    	
+         
          printf("Digite o usuario: ");
          scanf("%s", user);
 
@@ -28,13 +41,48 @@ int main(){
 		 	loginValido = 0;
 		 }
          
-         
     }while(loginValido == 0);
    
+	
+	do
+	{
+		
+		system("cls");
+		
+    	printf("===============================================\n");
+    	printf("================Sistema_Covid==================\n");
+    	printf("===============================================\n\n");
+    	
+    	printf("Selecione a opcao\n1 - gravar\n2- Registro\n3-Sair\nDigite a opcao: ");
+    	scanf("%d", &opcao);
+    	
+    	
+    	if(opcao == 1)
+		{	
+    		FILE *pont_arq;
+    		pont_arq = fopen("banco.txt", "a");
+    		
+    		printf("Digite o nome: ");
+    		scanf("%s", nome);
+    		
+    		fprintf(pont_arq, "%s", strcat(nome, "|"));
+    		
+    		fclose(pont_arq);
+    		
+    		
+		} else if(opcao == 2) { 
+			
+			
+		
+		}
+		
+    	system("PAUSE");
+		
+	}while(opcao != 3);
 
 
-    printf("digite alguma coisa para finalizar o sistema!");
-    scanf("%s", teste);
+
+    printf("Sistema finalizado ");
     
     return 0;
 }
